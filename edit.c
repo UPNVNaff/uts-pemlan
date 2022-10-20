084cd9a16848c22b67c8d7b23b3d8e33dcdabfee
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-10-20 23:28:11 +0700

    Update edit.c

--- edit.c
+++ edit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 void display();
 void frekuensi();
@@ -13,16 +14,21 @@ struct seagames{
 };
 int main(){
     int pilih;
-    printf("Pilih Untuk menampilkan jawaban dengan soal! \n");
+    menu:
+    printf("Pilih untuk menampilkan jawaban dengan soal! \n");
     printf("1. Simpan data diatas dalam bentuk struct yang memiliki 3 variabel, yaitu tahun, tuan rumah, dan kota\n");
-    printf("2. Buat tabel frekuensi untuk tabel diatas yang akhir tampilannya seperti berikut\n");
+    printf("2. Buatlah tabel frekuensi\n");
     printf("Pilihan : ");
     scanf("%d", &pilih);
     if (pilih == 1){
+        system("clear");
         display();
+        goto menu;
     }
     else if (pilih == 2){
+        system("clear");
         frekuensi();
+        goto menu;
     }
     else{
         printf("Pilihan tidak ada!");
@@ -93,19 +99,19 @@ void display(){
             19, 1997, "Indonesia", "Jakarta"
         },
         {
-            20, 1999, "Brunei    ", "Bandar seri begawan"
+            20, 1999, "Brunei    ", "Bandar Seri Begawan"
         },
         {
-            21, 2001, "Malaysia", "Kuala lumpur"
+            21, 2001, "Malaysia", "Kuala Lumpur"
         },
         {
-            22, 2003, "Vietnam", "Hanoi & Ho chi min"
+            22, 2003, "Vietnam", "Hanoi & Ho Chi Min"
         },
         {
             23, 2005, "Filipina", "Manila"
         },
         {
-            24, 2007, "Thailand", "Nakhon ratchasima"
+            24, 2007, "Thailand", "Nakhon Ratchasima"
         },
         {
             25, 2009, "Laos    ", "Vientiane"
@@ -120,7 +126,7 @@ void display(){
             28, 2015, "Singapura", "Singapura"
         },
         {
-            29, 2017, "Malaysia", "Kuala lumpur"
+            29, 2017, "Malaysia", "Kuala Lumpur"
         },
         {
             30, 2019, "Filipina", "Manila"
@@ -135,5 +141,36 @@ void display(){
 }
 
 void frekuensi(){
-    printf("tes");
+    char string[] = "Thailand Myanmar DIBATALKAN Malaysia Thailand Myanmar Malaysia Singapura Thailand Malaysia Indonesia Filipina Singapura Thailand Indonesia Malaysia Filipina Singapura Thailand Indonesia Brunei Malaysia Vietnam Filipina Thailand Laos Indonesia Myanmar Singapura Malaysia Filipina ";  
+    char words[20][20];  
+    int i = 0, j = 0, k, length, count;  
+      
+    for(k=0; string[k]!='\0'; k++){  
+        if(string[k] != ' ' && string[k] != '\0'){  
+            words[i][j++] = toupper(string[k]);  
+        }  
+        else{  
+            words[i][j] = '\0';  
+            i++;  
+            j = 0;  
+        }  
+    }  
+      
+    length = i+1;  
+    system("clear");
+    printf("Jumlah banyaknya tour tuan rumah!\n\n"); 
+    printf("Negara\t\t Jumlah\n");
+    printf("------------------------\n");
+    for(i = 0; i < length; i++){  
+        count = 1;  
+        for(j = i+1; j < length; j++){  
+           if(strcmp(words[i], words[j]) == 0 && (strcmp(words[j],"0") != 0)){  
+               count++;  
+               strcpy(words[j],"0");  
+           }   
+        } 
+        if(count > 1 ){
+            printf("%s  \t   %d\n", words[i], count); 
+        }
+    }  
 }
